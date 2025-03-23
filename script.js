230fe4bc9f8b782498980f7846b4a8d333619bf5
MarynaRomashca <marynarmnv@gmail.com>
2025-03-23 21:04:53 +0000

    100-1

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
@@ -6,7 +10,39 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+    
+    const card = document.createElement("div");
+    card.className = "episode-card";
+
+    const episodeName = document.createElement("h3");
+    const episodeCode = document.createElement("p");
+    const episodeImage = document.createElement("img");
+    const episodeText = document.createElement("div");
+
+    episodeName.textContent = episode.name;
+    episodeCode.textContent = `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`; // `S ${0,episode.number} E ${0,episode.season}`; //episode.id;
+    episodeImage.src = episode.image.medium;
+    episodeText.innerHTML = episode.summary;
+
+    rootElem.appendChild(episodeName);
+    rootElem.appendChild(episodeCode);
+    rootElem.appendChild(episodeImage);
+    rootElem.appendChild(episodeText);
+
+    card.appendChild(episodeName);
+    card.appendChild(episodeCode);
+    card.appendChild(episodeImage);
+    card.appendChild(episodeText);
+
+    rootElem.appendChild(card);
+  });
 }
+
+
+
+
 
 window.onload = setup;
